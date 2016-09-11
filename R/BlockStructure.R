d82cb41514311f02fc374047b4f1835efcd32db2
Kushal K Dey <kshldey@gmail.com>
2016-09-11 15:53:40 -0500

    ecostructure first package

---
+++ R/BlockStructure.R
@@ -0,0 +1,122 @@
+#' @title Ecological Block Struture plot with ggplot2 package
+#'
+#' @description Make a Structure Plot split into ecological blocks and ordered
+#' in each block by an ordering metadata
+#'
+#' @param omega Cluster membership probabilities of each sample. Usually a
+#' sample by cluster matrix in the Topic model output. The cluster weights
+#' sum to 1 for each sample.
+#' @param blocker_metadata a factor metadata used for creating blocks of Structure plots.
+#' @param order_metadata A quantitative metadata used for ordering sites in each block.
+#' @param palette A vector of colors assigned to the clusters. First color in
+#' the vector is assigned to the cluster labeled as 1, and second color in the
+#' vector is assigned to the cluster labeled as 2, etc. The number of colors
+#' must be the same or greater than the number of clusters. The clusters not
+#' assigned a color are filled with white in the figure. In addition, the
+#' recommended choice of color palette here is RColorBrewer, for instance
+#' RColorBrewer::brewer.pal(8, "Accent") or RColorBrewwer::brewer.pal(9, "Set1").
+#' @param yaxis_label Axis label for the samples.
+#' @param split_line Control parameters for line splitting the batches in the
+#' plot.
+#' @param axis_tick Control parameters for x-axis and y-axis tick sizes.
+#' @param plot_labels A logical parameter, if TRUE the function plots the axis labels.
+#' @param levels_decreasing if TRUE, the sites in the block are ordered in decreasing order
+#'                        of magnitude of ordering metadata. Default is TRUE.
+#' @param round_off The ordering metadata is rounded off to these many digits.
+#' @param layout The graph layout for plotting the block Structure output.
+#' @param panel_title_size The size of the title for layout panel
+#' @param panel_title_font The font of the title for the layout panel
+#' @param main_title the title of the Block Structure model plot.
+#'
+#'
+#' @return Plots the Block Structure plot visualization of the GoM model
+#'
+#' @examples
+#'
+#' library(HimalayanBirdsAbundance)
+#' data("HimalayanBirdsAbundance")
+#' library(Biobase)
+#' new_counts <- t(exprs(HimalayanBirdsAbundance));
+#' metadata <- pData(HimalayanBirdsAbundance);
+#' elevation_metadata=metadata$Elevation;
+#' east_west_dir = metadata$WorE;
+#' topic_clus <- maptpx::topics(new_counts, K=2, tol=0.1)
+#' omega <- topic_clus$omega
+#' BlockStructure(omega, blocker_metadata = east_west_dir,
+#'               order_metadata = elevation_metadata,
+#'               yaxis_label = "Elevation",
+#'               levels_decreasing = FALSE)
+#'
+#' @import CountClust
+#' @import grid
+#' @import gridExtra
+#' @import maptpx
+#' @import slam
+#'
+#' @export
+
+
+
+BlockStructure = function( omega,
+                           blocker_metadata,
+                           order_metadata,
+                           palette = RColorBrewer::brewer.pal(8, "Accent"),
+                           yaxis_label = "Y",
+                           split_line=list(split_lwd = 1,
+                                           split_col = "white"),
+                           axis_tick = list(axis_ticks_length = .1,
+                                            axis_ticks_lwd_y = .1,
+                                            axis_ticks_lwd_x = .1,
+                                            axis_label_size = 7,
+                                            axis_label_face = "bold"),
+                           plot_labels = TRUE,
+                           levels_decreasing=TRUE,
+                           round_off=1,
+                           layout=c(1,2),
+                           panel_title_size=10,
+                           panel_title_font=4,
+                           main_title="Block Structure Plot"){
+
+  split_indices <- split(1:dim(omega)[1], as.factor(blocker_metadata))
+  split_struct <- list()
+  for(l in 1:length(split_indices)){
+
+    elevation_split <- round(elevation_metadata[split_indices[[l]]], round_off);
+    omega_split <- omega[split_indices[[l]],]
+    if(levels_decreasing){
+      elevation_split_ordered <- elevation_split[order(elevation_split, decreasing=TRUE)]
+      omega_split_ordered <- omega_split[order(elevation_split, decreasing=TRUE),]
+    }else{
+      elevation_split_ordered <- elevation_split[order(elevation_split, decreasing=FALSE)]
+      omega_split_ordered <- omega_split[order(elevation_split, decreasing=FALSE),]
+    }
+    annotation <- data.frame(
+      sample_id = paste0("X", c(1:NROW(omega_split_ordered))),
+      tissue_label = factor(elevation_split_ordered,
+                            levels = unique(elevation_split_ordered) ) );
+
+    split_struct[[l]] <- CountClust::StructureGGplot(omega = omega_split_ordered,
+                                                     annotation = annotation,
+                                                     figure_title = names(split_indices)[l],
+                                                     palette = RColorBrewer::brewer.pal(8, "Accent"),
+                                                     yaxis_label = yaxis_label,
+                                                     split_line=split_line,
+                                                     order_sample = FALSE,
+                                                     axis_tick = axis_tick,
+                                                     plot_labels=TRUE)
+
+  }
+
+  if((layout[1]*layout[2]) != length(split_struct)){
+    stop("The layout size does not match with number of blocks")
+  }
+
+  do.call("grid.arrange",
+          args = list(grobs=split_struct,
+                      ncol = layout[2],
+                      nrow = layout[1],
+                      top=textGrob(main_title,
+                                   gp=gpar(fontsize=panel_title_size,
+                                           font=panel_title_font))))
+
+}
---
+++ R/CreateGlobalAbundanceFields.R
@@ -0,0 +1,123 @@
+#' @title Create global abundance dispersion field maps from local data frame
+#' and shapefiles
+#'
+#' @description Reads a data frame or a matrix of local site-species abundance,
+#' with rows being sites and columns being species, along with shapefiles for
+#' each of the bird species (may be more too) ocurring in the data file and then
+#' produces global dispersion field of abundance pattern for a user-specified
+#' latitudinal and longitudinal range for each site, driven by the species
+#' present in that site.
+#'
+#' @param local_data     abundance counts site-species matrix/data frame.
+#' @param shapefiles_dir The directory that contains shapefiles for all the
+#'                       species in the data
+#' @param raster_latlim The latitudinal range of the dispersion field
+#' @param raster_longlim the longitudinal range of the dispersion field
+#' @param raster_resolution The scale.resolution of the raster.
+#'                          The higher the reason, the finer the raster is
+#' @param base_local If the count of a species in a site is less than base_local,
+#'                    it is assumed to be absent from the site. The default is 0.
+#'                    Non zero values may result from obsrvational or data
+#'                    recording errors
+#' @param optional_species_names If the column name corresponding to some species
+#'                      does not match with the shapefiles, the user might need
+#'                      to replace it by another species close to it. In such a
+#'                      scenario, instead of using column names, the user can
+#'                      enter a new vector of names, which may be slight
+#'                      modification on the species from the column of the data,
+#'                      taking account of issues with non-matching bird species
+#'                      between shape files and the local data frame.
+#' @return Returns a list with the names of the elements of the list given by the
+#' row names of the input data matrix (sites). Each element of the list is the
+#' dispersion field matrix generated corresponding to the bird species present
+#' in the specific site to which it corresponds.
+#'
+#' @keywords counts data, local site-species data, global dispersion field
+#'
+#' @importFrom rgdal readOGR
+#' @import raster
+#' @importFrom utils stack
+#'
+#' @export
+
+CreateGlobalAbundanceFields = function(local_data,
+                                     shapefiles_dir=NULL,
+                                     raster_latlim = c(5,50),
+                                     raster_longlim = c(50,120),
+                                     raster_resolution = 5,
+                                     base_local = 2,
+                                     optional_species_names = NULL){
+
+
+  if(is.null(optional_species_names)){
+    species_names <- colnames(local_data)
+  }else{
+    species_names <- optional_species_names;
+  }
+
+  if(length(species_names)!=length(colnames(local_data))){
+    stop("The length of the optional species names vector must match with number of columns in local data")
+  }
+
+
+  dispersion.field <- list()
+  local.species<-list()
+  for (i in 1:length(row.names(local_data))){
+    local.list <- species_names[which(local_data[i,]>base_local)]
+    local.species[[i]]<-local.list
+  }
+
+  shapefile_names<-list.files(shapefiles_dir, pattern=".shp")
+
+
+  shapefile_namenos<- unlist(sapply(species_names,
+                                           function(x) grep(x, shapefile_names)))
+  ranges<-list()
+
+  for (j in 1:length(shapefile_namenos)){
+    cat("Reading shapefile for species", j, "\n")
+    suppressMessages(ranges[[j]]<-rgdal::readOGR(dsn=paste0(shapefiles_dir,"/",
+                                    shapefile_names[shapefile_namenos[j]]),
+                layer=sub(".shp",  "", shapefile_names[shapefile_namenos[j]])))}
+
+
+
+
+  for (z in 1:length(local.species)){
+    species_temp <- local.species[[z]]
+    matched_indices <- match(species_temp, species_names);
+
+    local_ranges <- lapply(1:length(matched_indices),
+                           function(l) return(ranges[[matched_indices[l]]]))
+
+     breedingranges<-local_ranges[sapply(local_ranges, function(x) sum(x@data$SEASONAL==1 | x@data$SEASONAL==2)>0)] #to remove shapefiles with no breeding ranges in them, where species are entered twice
+     breedingranges<-sapply(breedingranges, function(x) x[x@data$SEASONAL==1 | x@data$SEASONAL==2,])  ##extracts breeding ranges
+
+     xmin<-raster_longlim[1]
+     xmax<-raster_longlim[2]
+     ymin<-raster_latlim[1]
+     ymax<-raster_latlim[2]
+
+    r <- raster::raster(nrows=(ymax-ymin)*raster_resolution,
+                        ncols=(xmax-xmin)*raster_resolution,
+                        xmn=xmin,
+                        xmx=xmax,
+                        ymn=ymin,
+                        ymx=ymax)
+
+    r<- raster::setValues(r, 1)
+
+    a <- raster::mask(r, breedingranges[[1]], updatevalue=0)
+    for (i in 2:length(breedingranges)){
+      a<- utils::stack(a,mask(r, breedingranges[[i]], updatevalue=0))}
+      sum<-sum(a)
+
+      fun <- function(x) { x[x==0] <- NA; return(x) }
+      dispersion.field[[z]] <- as.matrix(raster::calc(sum, fun))
+      cat("Dispersion Field Map Complete for site:", z, "\n")
+  }
+
+  names(dispersion.field) <- rownames(local_data)
+  return(dispersion.field)
+}
+
