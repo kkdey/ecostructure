730530127032fbaf75b8b12deeb4b8a3687b84af
Kushal K Dey <kshldey@gmail.com>
2018-05-19 15:10:43 -0500

    added ecostructure_fit

---
+++ R/ecostructure_fit.R
@@ -0,0 +1,101 @@
+#' @title Fitting Grade of Membership model for clustering into communities
+#'
+#' @description A grade of membership model for clustering a site by features
+#' data - which could be presence-absence or counts abundance data of species
+#' in the sites.
+#' 
+#' @param dat input data matrix of samples along rows and features along sites,
+#'            with each entry a 0/1 signififying presence/absence or counts of
+#'            abundances. 
+#' @param K The number of clusters to fit
+#' @param tol The tolerance level of the model.
+#' @param fit_control The control parameters for the model.
+#' 
+#' @return Returns a model fit with \code{omega} as cluster membership 
+#'         probabilities and \code{theta} as cluster feature matrix.
+#'         
+#' @importFrom maptpx topics
+#' @importFrom methClust meth_topics
+#' @export
+
+
+
+ecostructure_fit <- function(dat,
+                             max_dat = NULL,
+                             K,
+                             tol = 0.1,
+                             fit_control = list()){
+  if(all(dat - floor(dat) != 0)){
+    stop("The matrix dat must be a matrix of integers - a binary or a counts matrix")
+  }
+  
+  ids_na <- which(is.na(dat))
+  if(length(ids_na) > 0){
+    warning("NAs in dat matrix: replaced by 0")
+    dat[is.na(dat)] = 0
+  }
+  
+  dat_by_2 <- dat %% 2
+  if(all(dat %% 2 - dat == 0)){
+    if(max(dat) == 1 & min(dat) == 0){
+      message("Binary matrix input: Fitting the Binomial Grade of Membership 
+              model.")
+      max_dat <- matrix(1, dim(dat)[1], dim(dat)[2])
+    }
+  }
+  
+  if(!is.null(max_dat)){
+    if(dim(max_dat)[1] != dim(dat)[1] | dim(max_dat)[2] != dim(dat)[2]){
+      stop("dimensions of max_dat should match with dat")
+    }
+    unmeth <- max_dat - dat
+    if(all(unmeth - floor(unmeth) != 0) | min(unmeth) < 0){
+      stop("max_dat is present: but data entries of max_dat must be integers 
+           bigger than the corresponding entries in the dat matrix.")
+    }
+    meth <- dat
+    
+    fit_control_default <-  list(shape=NULL,
+                                 initopics=NULL,
+                                 ord=TRUE,
+                                 verb=1,
+                                 sample_init = TRUE,
+                                 NUM_INDICES_START = floor(dim(dat)[1]/2),
+                                 use_squarem=FALSE)
+    
+    fit_control <- modifyList(fit_control_default, fit_control)
+    
+    topic_clus <- do.call(methClust::meth_topics,
+                          append(list(meth = meth, 
+                                      unmeth = unmeth, 
+                                      K = K, 
+                                      tol = tol), 
+                                      fit_control))
+    ll <- list("omega" = topic_clus$omega,
+               "theta" = topic_clus$theta,
+               "L" = topic_clus$L)
+  }
+  
+  if(is.null(max_dat) && max(dat) > 1){
+    
+    fit_control_default <-  list(shape = NULL, initopics = NULL, bf = TRUE, 
+                                 kill = 2, ord = TRUE, verb = 1, admix = TRUE, 
+                                 tmax = 1000)
+    fit_control <- modifyList(fit_control_default, fit_control)
+    
+    
+    topic_clus <- do.call(CountClust::FitGoM,
+                          list(data = dat,
+                               K = K,
+                               tol = tol,
+                               num_trials = 1,
+                               options = "BIC",
+                               path_rda = NULL,
+                               control = fit_control))
+
+    ll <- list("omega" = topic_clus$fit$omega,
+               "theta" = topic_clus$fit$theta,
+               "BIC" = topic_clus$BIC)
+  }
+  return(ll)
+}
---
+++ R/ecostructure_nullmodel.R
@@ -0,0 +1,132 @@
+#' @title Null models for ecostructure_fit clustering validation
+#'
+#' @description Generate randomized matrix of counts 
+#' given the observed data matrix for a particular null model, 
+#' run \code{ecostructure_fit} on these null matrices and compare the fit
+#' on null model data with that on the observed data. Used for validating 
+#' the clustering. 
+#'
+#' @param counts The counts matrix (N x G): N- the number of samples (sites), 
+#'               G- number of features (bird species)
+#' @param K  The number of clusters to fit
+#' @param tol The tolerance limit of the \code{ecostructure_fit}.
+#' @param null.model The type of nullmodel used (similar to the
+#'                   randomizeMatrix() function argument in picante package)
+#' @param iter_fill The number of swaps/fills in each randomized matrix build
+#' @param iter_randomized The number of randomization matrices generated
+#' @param plot If TRUE, plots density function of log Bayes factor over
+#'             the randomized iterations
+#'
+#' @return  Returns a list with
+#'        \item{BF.obs}{log Bayes Factor for the observed counts with 
+#'        K=2 against the null with no clusters}
+#'        \item{BF.rand}{a vector of log BF for each randomized count 
+#'        matrix with K=2 against the null with no clusters}
+#'        \item{pval}{the p-value of the observed log Bayes factor against the
+#'        ones from randomized matrices}
+#'
+#' @importFrom  picante randomizeMatrix
+#' @import slam
+#' @importFrom CountClust FitGoM
+#' @importFrom stats dmultinom density
+#' @export
+#'
+#' @examples
+#'
+#' data("ex.counts")
+#' nullmodel_GoM(ex.counts,
+#'               K=2,
+#'               tol=500,
+#'               null.model="frequency",
+#'               iter_randomized=3,
+#'               plot=FALSE)
+
+
+ecostructure_nullmodel <- function(counts,
+                                   K,
+                                   tol=0.1,
+                                   null.model=c("frequency", "richness",
+                                                "independentswap", "trialswap"),
+                                   iter_fill=100,
+                                   iter_randomized=30,
+                                   option,
+                                   plot=TRUE)
+{
+    if(missing(option)){
+      option = "BIC"
+    }
+    if(option != "BF" & option != "BIC"){
+      stop("The input argument option can only be BF (Bayes factor) or BIC")
+    }
+    counts <- counts[which(rowSums(counts) > 0), ];
+    bf_gom_rand <-
+        unlist(lapply(1:iter_randomized,
+                      function(n)
+                      {
+                          rand_counts <-
+                              picante::randomizeMatrix(counts,
+                                                       null.model=null.model,
+                                                       iterations=iter_fill);
+                          suppressMessages(
+                              topics_rand <- CountClust::FitGoM(rand_counts, 
+                                                                K=K, 
+                                                                tol=tol))
+                              if(option == "BF"){
+                                bf_gom  <- topics_rand$fit$BF
+                              }else{
+                                bf_gom <- topics_rand$BIC
+                              }
+                          return(bf_gom)
+                      }))
+
+    topics_obs <- suppressMessages(CountClust::FitGoM(counts, K=K, tol=tol));
+    
+    if(option == "BF"){
+        bf_gom_obs  <- topics_obs$fit$BF
+    }else{
+        bf_gom_obs <- topics_obs$BIC
+    }
+
+    pval_bf_gom <- length(which(bf_gom_obs < bf_gom_rand))/iter_randomized;
+
+    if(option == "BIC"){
+      ll <- list("BIC.obs"=bf_gom_obs,
+                 "BIC.rand"=bf_gom_rand,
+                 "pval"=pval_bf_gom)
+      if(plot){
+        plot(density(bf_gom_rand), col="blue",
+             main="BIC density over null matrices",
+             xlab="BIC", ylab="density",
+             xlim=c(min(bf_gom_obs,bf_gom_rand)-100, max(bf_gom_obs,bf_gom_rand)+100))
+        abline(v=bf_gom_obs, col="red")
+      }
+      return(ll)
+    }
+    if(option == "BF"){
+      ll <- list("BF.obs"=bf_gom_obs,
+                 "BF.rand"=bf_gom_rand,
+                 "pval"=pval_bf_gom)
+      if(plot){
+        plot(density(bf_gom_rand), col="blue",
+             main="log-BF density over null matrices",
+             xlab="log-BF", ylab="density",
+             xlim=c(min(bf_gom_obs,bf_gom_rand)-100, max(bf_gom_obs,bf_gom_rand)+100))
+        abline(v=bf_gom_obs, col="red")
+      }
+      return(ll)
+    }
+}
+
+
+CheckCounts <- function(counts){
+    if(class(counts)[1] == "TermDocumentMatrix"){ counts <- t(counts) }
+    if(is.null(dimnames(counts)[[1]]))
+    {dimnames(counts)[[1]] <- paste("doc",1:nrow(counts)) }
+    if(is.null(dimnames(counts)[[2]]))
+    { dimnames(counts)[[2]] <- paste("wrd",1:ncol(counts)) }
+    empty <- slam::row_sums(counts) == 0
+    if(sum(empty) != 0){
+        counts <- counts[!empty,]
+        cat(paste("Removed", sum(empty), "blank documents.\n")) }
+    return(slam::as.simple_triplet_matrix(counts))
+}
